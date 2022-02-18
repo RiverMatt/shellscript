19df4f249f6898e808c690cfdd33acadb0498ebd
Matthew Arnold <RiverMatt@pm.me>
2022-02-17 21:49:18 -0700

    Working slightly better

--- counting.sh
+++ counting.sh
@@ -6,11 +6,11 @@ count=0
 while [[ $count < 10 ]]; do
 	if [[ $x == 0 ]]; then
 		echo "x is exactly 0!"
-		$count++
-		$x=$count
+		((count++))
+		x=$count
 	else
 		echo "$x"
-		$count++
-		$x=$count
+		((count++))
+		x=$count
 	fi
 done
